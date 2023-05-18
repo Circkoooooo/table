04715dcff37b6fcc8fe7855657bb201088e0aee0
Circkoooooo <2389451262@qq.com>
2023-05-18 23:54:58 +0800

    fix: tool funciton arrToObject and test cases.

---
+++ src/tools/arrToObject.ts
@@ -0,0 +1,32 @@
+/**
+ * ['test'] => {'test':true}
+ * {'key':'value'} => {'key':'value'}
+ */
+export function arrToObject(arr: any[]) {
+  if (arr.length === 0) return {};
+
+  const objetArray = arr.map((item) => {
+    if (typeof item === "string" || item === "number") {
+      return {
+        [item]: true,
+      };
+    }
+
+    if (Object.prototype.toString.call(item) === "[object Object]") {
+      return {
+        ...item,
+      };
+    }
+
+    throw new Error("Need to receive a valid variable.");
+  });
+
+  const flatedObject = objetArray.reduce((pre, val) => {
+    return {
+      ...pre,
+      ...val,
+    };
+  });
+
+  return flatedObject;
+}
