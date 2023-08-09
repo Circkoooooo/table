d882dae0a670825de6e6058b43ce2a01f6a8bb3c
circkoooooo <2389451262@qq.com>
2023-08-09 23:56:04 +0800

    fix: add a deepClone tools function.

---
+++ src/tools/deepClone.ts
@@ -0,0 +1,30 @@
+const deepClone = (target: any) => {
+	if (target === null || target === undefined || !(typeof target === "object")) return target
+
+	//array
+	if (target instanceof Array) {
+		const tempArr: any = []
+
+		target.forEach((item) => {
+			tempArr.push(deepClone(item))
+		})
+
+		return tempArr
+	}
+
+	//object
+	const tempObject: any = {}
+	const propertySet = new Set()
+	for (const [key, value] of Object.entries(target)) {
+		propertySet.add(value)
+
+		if (propertySet.has(value)) {
+			tempObject[key] = value
+		} else {
+			tempObject[key] = deepClone(value)
+		}
+	}
+	return tempObject
+}
+
+export default deepClone
