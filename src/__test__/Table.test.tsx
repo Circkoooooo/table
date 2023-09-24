ac9816a8a9497c3859c706d2b63e943f5cc1ce4a
Circkoooooo <2389451262@qq.com>
2023-09-25 02:17:56 +0800

    chore: Clear deprecated test cases

--- src/__test__/Table.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react"
-import Table from "../core/Table"
-
-describe("Table", () => {
-	it("snapshot", () => {
-		const { asFragment } = render(<Table />)
-		// expect(asFragment()).toMatchSnapshot()
-	})
-})
--- src/__test__/TableBorder.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react"
-import TableBorder, { BorderWrapper } from "../core/components/TableBorder"
-
-describe("Table border component", () => {
-	it("border wrap component", () => {
-		const { asFragment } = render(<BorderWrapper width={100} height={100} $offsetLeft={10} $offsetTop={10} />)
-		expect(asFragment()).toMatchSnapshot()
-	})
-})
