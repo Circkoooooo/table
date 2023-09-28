8f5b2278a51e4a949994ebbb1c625d7ac326d276
Circkoooooo <2389451262@qq.com>
2023-09-28 17:00:53 +0800

    fix: Adapt to the recording cell data

--- src/core/redux/table-data/tableDataSlice.ts
+++ src/core/redux/table-data/tableDataSlice.ts
@@ -1,43 +1,23 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit"
-import { WithRulerCellData, createEmptyCellData, createRulerCellData } from "../../cellDataHandler"
+import { CellData, createEmptyCellData } from "../../cellDataHandler"
 import { DispatchInput } from "./tableDataSlice.types"
 
 type CellDataState = {
-	cellData: WithRulerCellData
+	cellData: CellData
 }
 
 const initialState: CellDataState = {
-	cellData: createRulerCellData(
-		createEmptyCellData({
-			rowNum: 26,
-			columnNum: 26,
-		})
-	),
+	cellData: createEmptyCellData({
+		rowNum: 26,
+		columnNum: 26,
+	}),
 }
 
 export const tableDataSlice = createSlice({
 	name: "table-data",
 	initialState,
 	reducers: {
-		inputDispatch: (state, action: PayloadAction<DispatchInput>) => {
-			const { cellIndex, newValue } = action.payload
-			const { rowIndex, columnIndex } = cellIndex
-
-			const newCellData = state.cellData.data.map((row, rowIdx) => {
-				if (rowIdx === rowIndex) {
-					return row.map((column, columnIdx) => {
-						if (columnIdx === columnIndex) {
-							return newValue
-						}
-						return column
-					})
-				}
-				return row
-			})
-
-			state.cellData.data = newCellData
-			return state
-		},
+		inputDispatch: (state, action: PayloadAction<DispatchInput>) => {},
 	},
 })
 
