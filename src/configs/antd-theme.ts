9ab23e373285341412a08843a9d7a3b975e97391
Circkoooooo <2389451262@qq.com>
2023-10-12 00:47:23 +0800

    add theme configures

---
+++ src/configs/antd-theme.ts
@@ -0,0 +1,11 @@
+import { ThemeConfig } from "antd"
+
+export const ANTD_THEME: ThemeConfig = {
+	token: {
+		borderRadius: 4,
+		lineWidth: 1,
+		colorPrimaryHover: "#8d8d8d",
+		colorPrimary: "#8d8d8d",
+		paddingSM: 0,
+	},
+}
