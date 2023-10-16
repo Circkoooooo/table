348256ef97dce2066f4d650b9b3adb0261fc3f6b
Circkoooooo <2389451262@qq.com>
2023-10-16 15:43:58 +0800

    fix: encapsulate extrasize function

---
+++ src/core/calcOffsetArr.ts
@@ -0,0 +1,31 @@
+import { TableRowColumnCellConfig } from "./redux/canvas/canvasSlice.types"
+import { CellDataInfoNumConfig } from "./redux/table-data/tableDataSlice"
+
+/**
+ * 获取当前渲染body部分所需要进行的偏移
+ * @param numConfig
+ * @param cellConfig
+ * @param dpr
+ */
+export const calcOffsetArr = (numConfig: CellDataInfoNumConfig, cellConfig: TableRowColumnCellConfig, dpr: number) => {
+	const { rowNum, columnNum } = numConfig
+	const { columnWidth, rowHeight } = cellConfig
+
+	let _currentSumRenderLeft = 0
+	let _currentSumRenderTop = 0
+
+	const leftOffsetArr: number[] = new Array(columnNum + 1).fill(0).map((_, index) => {
+		_currentSumRenderLeft += Math.round((columnWidth.find((item) => item.index === index - 1)?.value || 0) * dpr)
+		return _currentSumRenderLeft
+	})
+
+	const topOffsetArr: number[] = new Array(rowNum + 1).fill(0).map((_, index) => {
+		_currentSumRenderTop += Math.round((rowHeight.find((item) => item.index === index - 1)?.value || 0) * dpr)
+		return _currentSumRenderTop
+	})
+
+	return {
+		leftOffsetArr,
+		topOffsetArr,
+	}
+}
---
+++ src/core/calcSumExtraSize.ts
@@ -0,0 +1,18 @@
+import { TableRowColumnCellConfig } from "./redux/canvas/canvasSlice.types"
+
+/**
+ *根据rowColumnExtraSize的config数组，来分别输出row和column的额外渲染尺寸
+ */
+export const calcSumExtraSize = (extraSizeConfig: TableRowColumnCellConfig, dpr: number) => {
+	const { rowHeight, columnWidth } = extraSizeConfig
+
+	/**
+	 * 所有偏移量之和，即作为额外渲染尺寸
+	 */
+	const sumLeftExtra = Math.round(columnWidth.map((item) => item.value).reduce((pre, cur) => pre + cur, 0) * dpr)
+	const sumTopExtra = Math.round(rowHeight.map((item) => item.value).reduce((pre, cur) => pre + cur, 0) * dpr)
+	return {
+		sumLeftExtra,
+		sumTopExtra,
+	}
+}
